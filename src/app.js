bd007accdb0ba39ab8d4d62cd1f4aec60040c8a6
Matt Lee <matt@reddit.com>
2014-08-29 22:02:18 -0700

    Added Man obj.

--- src/app.js
+++ src/app.js
@@ -2,20 +2,32 @@
 function main() {
   var can = CannedVas.create();
   var app = document.getElementById('app');
-  app.appendChild(can.vas);
-
-  helloWorld(can);
-}
-
-function helloWorld(can) {
-  can.clear();
   can.size({
     width: 900,
     height: 600,
   });
   can.style('width', '450px');
   can.style('height', '300px');
-  can.font('normal normal 40px/40px Helvetica');
-  can.textBaseline('top');
-  can.fillText('Hello, world!', 10, 10);
+  app.appendChild(can.vas);
+
+  var man = new Man(50, 50);
+  man.draw(can);
+}
+
+function Man(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Man.prototype.height = 45;
+Man.prototype.width = 30;
+
+Man.prototype.draw = function(can) {
+  var x = this.x + this.width / 2;
+  var y = this.y + (this.height * 2 / 3);
+
+  can.translate(x, y)
+     .fillStyle('#222222')
+     .fillRect(0, 0, this.width, this.height)
+     .translate(-x, -y);
 }
